b4302c68f3ec08eff0ee21a687765f49ea130508
Michael <mkenton2020@icloud.com>
2021-07-28 12:22:07 -0500

    added pup info and name and reset functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const pupsUrl = 'http://localhost:3000/pups'
 
+//console.log(dogInfoDiv)
 
     fetch(pupsUrl)
     .then(r => r.json())
@@ -18,9 +19,31 @@ const pupsUrl = 'http://localhost:3000/pups'
     //console.log(dogBar)
     //console.log(eachPup)
     const bttn = document.createElement('button')
-    bttn.textContent = eachPup.name
+    
     dogBar.append(bttn)
-
+    const span = document.createElement('span')
+    span.textContent = eachPup.name
+    span.id = eachPup.id
+    bttn.append(span)
+    bttn.addEventListener('click', function(e){
+    showPupInfo(eachPup)
+    console.log(eachPup.id)
+    })
+    
+    
+    //addPupInfo()
     
     //console.log(eachPup.name)
+}
+
+function showPupInfo(pupInfo){
+    const dogInfoDiv = document.getElementById('dog-info')
+    dogInfoDiv.innerHTML = ''
+    const img = document.createElement('img')
+    img.src = pupInfo.image
+    dogInfoDiv.append(img)
+    const h2 = document.createElement('h2')
+    h2.textContent = pupInfo.name
+    dogInfoDiv.append(h2)
+    console.log(pupInfo.name)
 }
