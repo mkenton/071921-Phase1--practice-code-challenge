a8e663b8826e23d9c7995679923b1587f9a707aa
Michael <mkenton2020@icloud.com>
2021-07-28 12:57:18 -0500

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ const pupsUrl = 'http://localhost:3000/pups'
     .then(pups => {
     //console.log(pups)
     pups.forEach(pup => addPupButton(pup))
+
+//  //add good dog fitler functionatlity
+    dogFilter()   
 })
 
 })
@@ -32,18 +35,66 @@ const pupsUrl = 'http://localhost:3000/pups'
     
     
     //addPupInfo()
-    
     //console.log(eachPup.name)
 }
 
 function showPupInfo(pupInfo){
     const dogInfoDiv = document.getElementById('dog-info')
+    //reset div before each display on click
     dogInfoDiv.innerHTML = ''
+    //add img of dog to info div
     const img = document.createElement('img')
     img.src = pupInfo.image
     dogInfoDiv.append(img)
+    //add dog name to dog info div
     const h2 = document.createElement('h2')
     h2.textContent = pupInfo.name
     dogInfoDiv.append(h2)
-    console.log(pupInfo.name)
+    //console.log(pupInfo.name)
+    //add Good dog/bad dog button
+    const bttnGB = document.createElement('button')
+    bttnGB.id = "goodOrBad"
+    //console.log(pupInfo.isGoodDog)
+    if (pupInfo.isGoodDog === true) {
+        bttnGB.textContent = 'Good Dog!';
+        bttnGB.value = "good"
+    } else {bttnGB.textContent = 'Bad Dog!'
+        bttnGB.value = "bad"
+    }
+    
+    dogInfoDiv.append(bttnGB)
+    bttnGB.addEventListener('click', switchValue)
+    
+}
+
+function switchValue(e){
+    console.log(e.target)
+    let dogBtn = document.querySelector('#goodOrBad')
+    console.log(dogBtn)
+    if (dogBtn.value === "good"){
+        dogBtn.textContent = 'Bad Dog!'
+        dogBtn.value = "bad"
+    } else {
+            dogBtn.textContent = 'Good Dog!'
+            dogBtn.value = "good"
+    }
+    console.log(dogBtn.value)
+}
+
+
+function dogFilter(){
+    let filter = document.querySelector('#good-dog-filter')
+    filter.addEventListener('click', filterDogs)
+}
+
+function filterDogs(e){ 
+    let filter = document.querySelector('#good-dog-filter')
+    if (filter.textContent === "Filter good dogs: OFF"){
+        filter.textContent = "Filter good dogs: ON"
+        filter.value = "on"
+    } else {
+        filter.textContent = "Filter good dogs: OFF"
+        filter.value = "off"
+    }
+    console.log(e.target.textContent)
 }
